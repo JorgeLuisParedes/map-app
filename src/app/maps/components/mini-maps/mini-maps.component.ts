d45da68809ae6daaf2d2e3d02bab367114a058c9
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-19 02:52:15 +0200

    Componente MiniMapa

--- src/app/maps/components/mini-maps/mini-maps.component.ts
+++ src/app/maps/components/mini-maps/mini-maps.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-maps',
-  templateUrl: './mini-maps.component.html',
-  styleUrl: './mini-maps.component.css'
+	selector: 'map-mini-maps',
+	templateUrl: './mini-maps.component.html',
+	styleUrl: './mini-maps.component.css',
 })
-export class MiniMapsComponent {
+export class MiniMapsComponent implements AfterViewInit {
+	@Input() lngLat?: [number, number];
 
+	@ViewChild('map') divMap?: ElementRef;
+
+	ngAfterViewInit(): void {
+		if (!this.divMap) throw 'Map Div not found';
+		if (!this.lngLat) throw "LngLat can't be null";
+
+		const map = new Map({
+			container: this.divMap?.nativeElement, // container ID
+			style: 'mapbox://styles/mapbox/streets-v12', // style URL
+			center: this.lngLat, // starting position [lng, lat]
+			zoom: 15, // starting zoom
+			interactive: false,
+		});
+
+		new Marker().setLngLat(this.lngLat).addTo(map);
+	}
 }
