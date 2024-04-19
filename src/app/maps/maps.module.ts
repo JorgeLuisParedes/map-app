bcb9b9c501f503116ce8ba96c60bd69ea080003f
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-19 09:59:00 +0200

    Standalone - en módulos tradicionales

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -12,6 +12,7 @@ import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-pa
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
+import { CounterAloneComponent } from '../alone/components/counter-alone/counter-alone.component';
 
 @NgModule({
 	declarations: [
@@ -23,6 +24,6 @@ import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.
 		PropertiesPageComponent,
 		ZoomRangePageComponent,
 	],
-	imports: [CommonModule, MapsRoutingModule],
+	imports: [CommonModule, MapsRoutingModule, CounterAloneComponent],
 })
 export class MapsModule {}
